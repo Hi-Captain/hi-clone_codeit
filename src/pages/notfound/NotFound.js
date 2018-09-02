51bac819bbfa58092804737d499474e6939bca2a
hi_captain_ <7273captain@gmail.com>
2018-09-02 23:31:49 +0900

    NotFound Page

--- src/pages/notfound/NotFound.js
+++ src/pages/notfound/NotFound.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
+import './NotFound.scss';
 
-const NotFound = () => {
-  return (
-    <div>
-      NotFound
-    </div>
-  );
-};
+class NotFound extends Component {
+  componentDidMount() { 
+    const delList = document.querySelectorAll('.navbar, .footer, .chat-plugin')
+    for(var el of delList){el.remove()}
+  }
+  
+  render() {
+    return (
+      <div className="notfound">
+        <div className="dialog">
+          <div>
+            <h1>The page you were looking for doesn't exist.</h1>
+            <p>You may have mistyped the address or the page may have moved.</p>
+          </div>
+          <p>If you are the application owner check the logs for more information.</p>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default NotFound;
